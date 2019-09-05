14398166d5d26f57a53022013dc760029d48a553
sshanmu91 <36123879+sshanmu91@users.noreply.github.com>
2019-09-06 01:24:26 +0530

    Lunch Events Mapper

---
+++ js/lunch-matcher.js
@@ -0,0 +1,68 @@
+function matchLunchEvent(availableLunchEvents) {
+    var selfPerson = [],
+        suitableListedPersons = [],
+        notSuitableListedPersons = [],
+        suitablePersons = [],
+        notSuitablePersons = [],
+        lunchMatchScheduler = {},
+        isSuitableMatchAvailable = false;
+    
+    selfPerson.push(availableLunchEvents[0]);
+    
+    for (var i = 1; i < availableLunchEvents.length; i += 1) {
+        var overlapTime = 0;
+        
+        if (availableLunchEvents[i].start <= availableLunchEvents[0].start) {
+            if (availableLunchEvents[i].end > availableLunchEvents[0].start) {
+                overlapTime = availableLunchEvents[i].end - availableLunchEvents[0].start;
+            }
+        } else if (availableLunchEvents[i].start > availableLunchEvents[0].start) {
+            if (availableLunchEvents[0].end > availableLunchEvents[i].start) {
+                overlapTime = availableLunchEvents[0].end - availableLunchEvents[i].start;
+            }
+        }
+        
+        if (overlapTime >= 30) {
+            suitableListedPersons.push(availableLunchEvents[i]);
+        } else {
+            notSuitableListedPersons.push(availableLunchEvents[i]);
+        }
+    }
+    
+    if (suitableListedPersons.length > 0) {
+        suitableListedPersons.sort(function (personA, personB) {return personA.start - personB.start});
+        
+        isSuitableMatchAvailable = true;
+        suitablePersons.push(suitableListedPersons[0]);
+        
+        for (i = 1; i < suitableListedPersons.length; i += 1) {
+            notSuitableListedPersons.push(suitableListedPersons[i]);
+        }
+    }
+    
+    notSuitableListedPersons.sort(function (personA, personB) {return personA.start - personB.start});
+    
+    notSuitablePersons.push(notSuitableListedPersons[0]);
+    for (i = 1; i < notSuitableListedPersons.length; i += 1) {
+        if (notSuitableListedPersons[i].start >= notSuitablePersons[notSuitablePersons.length - 1].end) {
+            notSuitablePersons.push(notSuitableListedPersons[i]);
+        } else {
+            suitablePersons.push(notSuitableListedPersons[i]);
+        }
+    }
+    
+    lunchMatchScheduler.isSuitableMatchAvailable = isSuitableMatchAvailable;
+    lunchMatchScheduler.notSuitablePersons = notSuitablePersons;
+    lunchMatchScheduler.suitablePersons = suitablePersons;
+    lunchMatchScheduler.selfPerson = selfPerson;
+    
+    LunchEventMapper.renderLunchEvents(lunchMatchScheduler);
+}
+
+//Suitable Lunch Match
+var availableLunchEvents = [{start: 225, end: 285},{start: 210, end: 270},{start: 180, end: 240},{start: 240, end: 300},{start: 300, end: 360},{start: 270, end: 330}];
+
+//No Suitable Lunch Match
+//var availableLunchEvents = [{start: 225, end: 285},{start: 300, end: 360}, {start: 180, end: 240}];
+
+matchLunchEvent(availableLunchEvents);
---
+++ js/main.js
@@ -0,0 +1,118 @@
+function LunchEventMapper () {};
+
+LunchEventMapper.renderLunchEvents = function(lunchMatchScheduler) {
+    LunchEventMapper.resetLunchEvents();
+    
+    var lunchEventElement = '<li class="lunch-schedule-event"><span>Brilliant Lunch</span></li>';
+    var lunchEventsDOM = '';
+    var lunchEventsArray = [];
+    
+    lunchEventsArray = lunchMatchScheduler.notSuitablePersons;
+    for (var i = 0; i < lunchEventsArray.length; i += 1) {
+        lunchEventsDOM += lunchEventElement;
+    }
+    $('.not-suitable-persons-events').html(lunchEventsDOM);
+    
+    lunchEventsArray = lunchMatchScheduler.suitablePersons;
+    if (lunchEventsArray.length > 0) {
+        lunchEventsDOM = '';
+        for (i = 0; i < lunchEventsArray.length; i += 1) {
+            lunchEventsDOM += lunchEventElement;
+        }
+        $('.suitable-persons-events').html(lunchEventsDOM);
+    } else {
+        $('.suitable-persons-events').parent().hide();
+    }
+    
+    lunchEventsDOM = '';
+    lunchEventElement = '<li class="lunch-schedule-event"><span>Me</span></li>';
+    lunchEventsArray = lunchMatchScheduler.selfPerson;
+    for (i = 0; i < lunchEventsArray.length; i += 1) {
+        lunchEventsDOM += lunchEventElement;
+    }
+    $('.my-event').html(lunchEventsDOM);
+    
+	LunchEventMapper.sizeLunchEvents(lunchMatchScheduler);
+};
+
+LunchEventMapper.sizeLunchEvents = function (lunchMatchScheduler) {
+    var slotHeight = 30;
+    var timelineStart = 0;
+    var timelineUnitDuration = 30;
+    var lunchEventsArray = [];
+    var start = 0,
+        duration = 0,
+        eventTop = 0,
+        eventHeight = 0;
+    
+    lunchEventsArray = lunchMatchScheduler.notSuitablePersons;
+    for (var i = 0; i < lunchEventsArray.length; i += 1) {
+        start = lunchEventsArray[i].start;
+        duration = lunchEventsArray[i].end - start;
+        eventTop = (slotHeight * (start - timelineStart)) / timelineUnitDuration;
+        eventHeight = (slotHeight * duration) / timelineUnitDuration;
+        
+        $($('.not-suitable-persons-events').find('.lunch-schedule-event')[i]).attr('style', ('top: ' + (eventTop - 1) + 'px; height: ' + (eventHeight + 1) + 'px'));
+    }
+    
+    lunchEventsArray = lunchMatchScheduler.suitablePersons;
+    if (lunchEventsArray.length > 0) {
+        for (i = 0; i < lunchEventsArray.length; i += 1) {
+            start = lunchEventsArray[i].start;
+            duration = lunchEventsArray[i].end - start;
+            eventTop = (slotHeight * (start - timelineStart)) / timelineUnitDuration;
+            eventHeight = (slotHeight * duration) / timelineUnitDuration;
+
+            $($('.suitable-persons-events').find('.lunch-schedule-event')[i]).attr('style', ('top: ' + (eventTop - 1) + 'px; height: ' + (eventHeight + 1) + 'px'));
+        }
+        
+        if (lunchMatchScheduler.isSuitableMatchAvailable) {
+            $($('.suitable-persons-events').find('.lunch-schedule-event')[0]).addClass("available-matcher");
+        }
+    }
+    
+    lunchEventsArray = lunchMatchScheduler.selfPerson;
+    for (i = 0; i < lunchEventsArray.length; i += 1) {
+        start = lunchEventsArray[i].start;
+        duration = lunchEventsArray[i].end - start;
+        eventTop = (slotHeight * (start - timelineStart)) / timelineUnitDuration;
+        eventHeight = (slotHeight * duration) / timelineUnitDuration;
+        
+        $($('.my-event').find('.lunch-schedule-event')[i]).attr('style', ('top: ' + (eventTop - 1) + 'px; height: ' + (eventHeight + 1) + 'px'));
+        
+        if (lunchMatchScheduler.isSuitableMatchAvailable) {
+            $($('.my-event').find('.lunch-schedule-event')[0]).addClass("available-matcher");
+        } else {
+            $($('.my-event').find('.lunch-schedule-event')[0]).addClass("no-suitable-matcher-available");
+        }
+    }
+    
+    var lunchEventWidth = Math.floor($($('.my-event').find('.lunch-schedule-event')[0]).outerWidth());
+    
+    if (lunchMatchScheduler.suitablePersons.length > 0) {
+        if (lunchMatchScheduler.notSuitablePersons[lunchMatchScheduler.notSuitablePersons.length - 1].start >= lunchMatchScheduler.suitablePersons[lunchMatchScheduler.suitablePersons.length - 1].end) {
+            if (lunchMatchScheduler.notSuitablePersons[lunchMatchScheduler.notSuitablePersons.length - 1].start >= lunchMatchScheduler.selfPerson[lunchMatchScheduler.selfPerson.length - 1].end) {
+                $($('.not-suitable-persons-events').find('.lunch-schedule-event')[lunchMatchScheduler.notSuitablePersons.length - 1]).css('width', ((3 * lunchEventWidth) + 'px'));
+            } else {
+                $($('.not-suitable-persons-events').find('.lunch-schedule-event')[lunchMatchScheduler.notSuitablePersons.length - 1]).css('width', ((2 * lunchEventWidth) + 'px'));
+            }
+        } else {
+            if (lunchMatchScheduler.suitablePersons[lunchMatchScheduler.suitablePersons.length - 1].start >= lunchMatchScheduler.selfPerson[lunchMatchScheduler.selfPerson.length - 1].end) {
+                $($('.suitable-persons-events').find('.lunch-schedule-event')[lunchMatchScheduler.suitablePersons.length - 1]).css('width', ((2 * lunchEventWidth) + 'px'));
+            }
+        }
+    } else {
+        if (lunchMatchScheduler.notSuitablePersons[lunchMatchScheduler.notSuitablePersons.length - 1].start >= lunchMatchScheduler.selfPerson[lunchMatchScheduler.selfPerson.length - 1].end) {
+            $($('.not-suitable-persons-events').find('.lunch-schedule-event')[lunchMatchScheduler.notSuitablePersons.length - 1]).css('width', ((2 * lunchEventWidth) + 'px'));
+        }
+    }
+};
+
+LunchEventMapper.resetLunchEvents = function () {
+    $('.not-suitable-persons-events').empty();
+    
+    $('.suitable-persons-events').empty();
+    $('.suitable-persons-events').parent().show();
+    
+    $('.my-event').empty();
+};
